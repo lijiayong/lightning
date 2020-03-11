6a8d639e27ed4c3a3f75c6fd80d39a437c4a53e9
Tom Clegg <tom@tomclegg.ca>
2020-03-11 10:20:33 -0400

    Add -priority to example script.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@tomclegg.ca>

--- example-su92l-1kg.sh
+++ example-su92l-1kg.sh
@@ -14,26 +14,12 @@ gvcf=su92l-4zz18-bgyq36m6gctk63q
 info=su92l-4zz18-ykpcoea5nisz74f
 tags=su92l-4zz18-92bx4zjg5hgs3yc
 
-genome=$(lightning ref2genome -project ${project} -priority ${priority} -ref ${ref_fa})
-fasta=$(lightning vcf2fasta   -project ${project} -priority ${priority} -ref ${ref_fa} -genome ${genome} -mask=true ${gvcf})
-unfiltered=$(
-    lightning import       -project ${project} \
-       -tag-library ${tags}/tagset.fa.gz \
-       ${fasta})
-filtered=$(
-    lightning filter       -project ${project} \
-       -i ${unfiltered} \
-       -min-coverage 0.9 -max-variants 30)
-numpy=$(
-    lightning export-numpy -project ${project} \
-       -i ${filtered})
-pca=$(
-    lightning pca          -project ${project} \
-       -i ${numpy})
-plot=$(
-    lightning plot         -project ${project} \
-       -i ${pca} \
-       -labels-csv ${info}/sample_info.csv \
-       -sample-fasta-dir ${fasta})
+genome=$(lightning     ref2genome   -project ${project} -priority ${priority} -ref ${ref_fa})
+fasta=$(lightning      vcf2fasta    -project ${project} -priority ${priority} -ref ${ref_fa} -genome ${genome} -mask=true ${gvcf})
+unfiltered=$(lightning import       -project ${project} -priority ${priority} -tag-library ${tags}/tagset.fa.gz           ${fasta})
+filtered=$(lightning   filter       -project ${project} -priority ${priority} -i ${unfiltered} -min-coverage "0.9" -max-variants "30")
+numpy=$(lightning      export-numpy -project ${project} -priority ${priority} -i ${filtered})
+pca=$(lightning        pca          -project ${project} -priority ${priority} -i ${numpy})
+plot=$(lightning       plot         -project ${project} -priority ${priority} -i ${pca} -labels-csv ${info}/sample_info.csv -sample-fasta-dir ${fasta})
 echo >&2 "https://workbench2.${plot%%-*}.arvadosapi.com/collections/${plot}"
 echo ${plot%%/*}
